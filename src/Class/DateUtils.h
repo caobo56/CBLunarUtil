81cd826fedd347bfc305789c781fc8b839ff0a52
caobo <caobo56@sina.com>
2017-12-20 15:57:08 +0800

    添加LJsonFileUtils 和 DateUtils 的接口注释

--- src/Class/DateUtils.h
+++ src/Class/DateUtils.h
@@ -10,33 +10,110 @@
 
 @interface DateUtils : NSObject
 
+
+/**
+ 获取当前日期星期几的NSInteger值
+
+ @param inputDate 输入日期
+ @return return value 返回星期几数值
+ */
 +(NSInteger)numberOfWeekIn:(NSDate *)inputDate;
 
+/**
+ 获取当前日期年数的NSInteger值
+
+ @param inputDate 输入日期
+ @return return value 返回年数数值
+ */
 +(NSInteger)numberOfYearIn:(NSDate *)inputDate;
 
+/**
+ 获取当前日期月数的NSInteger值
+ 
+ @param inputDate 输入日期
+ @return return value 返回月数数值
+ */
 +(NSInteger)numberOfMonthIn:(NSDate *)inputDate;
 
+/**
+ 获取当前日期天数的NSInteger值
+ 
+ @param inputDate 输入日期
+ @return return value 返回天数数值
+ */
 +(NSInteger)numberOfDayIn:(NSDate *)inputDate;
 
+/**
+ 获取当前日期小时数的NSInteger值
+ 
+ @param inputDate 输入日期
+ @return return value 返回小时数数值
+ */
 +(NSInteger)numberOfHourIn:(NSDate *)inputDate;
 
+/**
+ 获取当前日期农历年数的NSInteger值
+ 
+ @param inputDate 输入日期
+ @return return value 返回农历年数数值
+ */
 +(NSInteger)numberOfChinaYearIn:(NSDate *)inputDate;
 
+/**
+ 获取当前日期农历月数的NSInteger值
+ 
+ @param inputDate 输入日期
+ @return return value 返回农历月数数值
+ */
 +(NSInteger)numberOfChinaMonthIn:(NSDate *)inputDate;
 
+/**
+ 获取当前日期农历天数的NSInteger值
+ 
+ @param inputDate 输入日期
+ @return return value 返回农历天数数值
+ */
 +(NSInteger)numberOfChinaDayIn:(NSDate *)inputDate;
 
+/**
+ 获取当前日期后一天的时间
+
+ @param inputDate 输入日期
+ @return return value 返回当后日期前一天的时间
+ */
 +(NSDate *)theAfterDay:(NSDate *)inputDate;
 
+/**
+ 获取当前日期前一天的时间
+ 
+ @param inputDate 输入日期
+ @return return value 返回当前日期前一天的时间
+ */
 +(NSDate *)theBeforeDay:(NSDate *)inputDate;
 
+/**
+ 获取当前日期后一年的时间
+ 
+ @param inputDate 输入日期
+ @return return value 返回当前日期后一年的时间
+ */
 +(NSDate *)theAfterMonth:(NSDate *)inputDate;
 
+/**
+ 获取当前日期前一年的时间
+ 
+ @param inputDate 输入日期
+ @return return value 返回当前日期前一年的时间
+ */
 +(NSDate *)theBeforeMonth:(NSDate *)inputDate;
 
-/****
- ios比较日期大小默认会比较到天
- ****/
+/**
+ ios比较日期大小，只比较到天
+
+ @param oneDay oneDay 一个日期
+ @param anotherDay anotherDay 另一个日期
+ @return return value NSComparisonResult
+ */
 +(NSComparisonResult)compareOneDay:(NSDate *)oneDay withAnotherDay:(NSDate *)anotherDay;
 
 /**
@@ -55,4 +132,5 @@
  @return return date
  */
 +(NSDate *)dateToDay:(NSDate *)inputDate;
+
 @end
--- src/Class/LJsonFileUtils.h
+++ src/Class/LJsonFileUtils.h
@@ -10,6 +10,12 @@
 
 @interface LJsonFileUtils : NSObject
 
+/**
+ 将 .json文件内容，读取为 dict or Arr
+
+ @param name name 文件名
+ @return return value id对象（dict or Arr）
+ */
 +(id)dataWithJsonFile:(NSString *)name;
 
 @end
